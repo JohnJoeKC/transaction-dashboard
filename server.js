1310ed324b0b8152e4cd7ce269d1e2e0db8381a3
JJ <jjcasey4@hotmail.com>
2023-04-20 17:07:22 +0100

    renamed app

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'transaction-dashboard')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'transaction-dashboard', 'transaction-dashboard-tests.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/execute', (req, res) => {
