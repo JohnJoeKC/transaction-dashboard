815a35eebceb322c74b1289e7ef32261cead48e2
JJ <jjcasey4@hotmail.com>
2023-04-20 18:31:14 +0100

    tests page spelling fix in server,js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'transaction-dashboard')));
 
 app.get('/tests-page/transaction-dashboard-tests.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'transaction-dashboard', 'test-page', 'transaction-dashboard-tests.html'));
+  res.sendFile(path.join(__dirname, 'transaction-dashboard', 'tests-page', 'transaction-dashboard-tests.html'));
 });
 
 app.post('/execute', (req, res) => {
