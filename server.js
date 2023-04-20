9db968994dd9bf61c7855d33e528db997ea674dd
JJ <jjcasey4@hotmail.com>
2023-04-20 17:20:04 +0100

    Fix test page routing

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'transaction-dashboard')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/tests-page/transaction-dashboard-tests.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'transaction-dashboard', 'test-page', 'transaction-dashboard-tests.html'));
 });
+
 
 app.post('/execute', (req, res) => {
   // You can remove this endpoint if you don't need server-side code execution
